09e115f779a87a2ab80aeff36738c903d0b23ba0
Franciele Zanella <36767316+Franzanella@users.noreply.github.com>
2020-03-27 14:56:40 -0300

    Funcionalidades avançadas - Testes

--- projeto/backend/knexfile.js
+++ projeto/backend/knexfile.js
@@ -16,6 +16,23 @@ module.exports = {
   },
 
 
+  test: {
+    client: 'sqlite3',
+    connection: {
+      filename: './src/database/test.sqlite'
+    },
+
+    migrations: {
+      directory: './src/database/migrations'
+    },
+    useNullAsDefault: true,
+
+  },
+
+
+
+
+
 
   staging: {
     client: 'postgresql',
